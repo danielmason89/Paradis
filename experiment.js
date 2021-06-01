d16973934e91b112f21853eaa40fb531a7b69eef
Daniel Mason <77700361+danielmason89@users.noreply.github.com>
2021-06-01 15:57:54 -0400

    Add files via upload

---
+++ experiment.js
@@ -0,0 +1,9 @@
+// const controller1 = new ScrollMagic.Controller();
+
+// const exploreScene = new ScrollMagic.Scene({
+//         triggerElement: '.hike-exp',
+//         triggerHook: 0.8
+//     })
+//     .addIndicators({ colorStart: 'white', colorTrigger: 'white' })
+//     .setClassToggle('.hike-exp', 'active')
+//     .addTo(controller1);
